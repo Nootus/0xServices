09add6558955ffc18c888e592ad520c104f8dc3b
Prasanna Kumar Pattam <pprasannak@hotmail.com>
2017-08-23 08:16:20 +0530

    Added comments

--- Contracts/Service.sol
+++ Contracts/Service.sol
@@ -1,102 +1,115 @@
 pragma solidity ^0.4.14;
 
+/// @title contract between client and contractor. This is generated contract
 contract Service {
     address client;
     address contractor;
     address platform; // 0x is the owner every contract
 
-    uint amount;
-    uint budget;
+    uint budget; // amount budgeted for the service
+    uint amountPaid; // amount paid to the contractor
 
+    // enum to define states of the service contract
     enum State { Bid, Award, Work, Paid }
-    State contractState;
+    State contractState; // to store the current state of the service contract
 
     // struct type to hold bidder information
     struct Bidder {
         address contractor;
         uint bidAmount;
     }
-    mapping(address => Bidder) bids;
+    mapping(address => Bidder) bids; // storing all bids received for the contract
 
-    // creates the service for the client
-    // 0x will be platform provider
+    /// @notice constructor creating service for the client
+    /// @param 0x will the platform provider
     function Service (address _platform) {
         client = msg.sender;
         platform = _platform;
     }
 
-    // modifier to ensure client can only execute a function
+    /// @notice modifier to ensure client can only execute a function
     modifier onlyClient() {
         require(msg.sender == client);
         _;
     }
 
-    // modifier to ensure contractor can only execute
+    /// @notice modifier to ensure contractor can only execute
     modifier onlyContractor() {
         require(msg.sender == contractor);
         _;
     }    
 
-    // modifier to ensure 0x can only perform
+    /// @notice modifier to ensure 0x can only perform
     modifier onlyPlatform() {
         require(msg.sender == platform);
         _;
     }    
 
+    /// @notice modifier to check for current state before proceeding
     modifier inState(State state) {
         require(contractState == state);
         _;
     }
 
-    // modifier for client to perform actions, 0x can also perform these actions
-    // modifier onlyClientOrPlatform() {
-    //     require(msg.sender == platform || msg.sender == client);
-    //     _;
-    // }    
+    /// @notice modifier for client to perform actions, 0x can also perform these actions
+    modifier onlyClientOrPlatform() {
+        require(msg.sender == platform || msg.sender == client);
+        _;
+    }    
 
-    // hiring the contractor to perform the work
+    /// @notice hiring the contractor to perform the work
+    /// @param ethereum address of the contractor
     function hire (address _contractor) onlyClient {
         contractor = _contractor;
     }
 
-    // based on the agreement, client will fund the contract
-    // this amount will be payed to contractor at the end
+    /// @notice Get the contractor assigned to the service
+    /// @return contractor address
+    function getContractorAddress() public constant returns (address) {
+        return contractor;
+    }
+
+    /// @notice based on the agreement, client will fund the contract
+    /// this amount will be payed to contractor at the end
     function fund() onlyClient payable {
-        amount += msg.value;
+        amountPaid += msg.value;
     }
 
-    // once the service is completed, client pays to the contractor
+    /// @notice Once the service is completed, client pays to the contractor
     function pay() onlyClient {
-        contractor.transfer(amount);
-        amount = 0;
+        contractor.transfer(amountPaid);
+        amountPaid = 0;
     }
 
-    // validate the terms of the contract
+    /// @notice Validate the terms of the contract
     function validate() constant returns (bool) {
         // for POC we are doing additional validations
         return true;
     }
 
-    function arbitrate (uint contractorAmount) onlyPlatform {
+    /// @notice 0x performs the dispute settlement and transfers money accordingly
+    /// @param amount to be paid to contractor, remaining amount will be paid to client
+    function disputeSettlement (uint contractorAmount) onlyPlatform {
         if (contractorAmount > 0) {
             contractor.transfer(contractorAmount);
-            amount -= contractorAmount;
+            amountPaid -= contractorAmount;
         }
         
         // kill the contractor once it is arbitrated
         kill();
     }
 
+    /// @notice close the contract once service is rendered and payment is made
     function kill() onlyPlatform {
-        if (amount > 0) {
-            client.transfer(amount);
+        if (amountPaid > 0) {
+            client.transfer(amountPaid);
         }
         selfdestruct(client);
     }
 
-    //default function
+    /// @notice fallback function which will revert the call as we don't allow it
     function() {
-        throw;
+        revert();
     }    
 }
 
