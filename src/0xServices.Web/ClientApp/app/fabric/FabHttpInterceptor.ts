4461188d3d659ecd08efc16a34e32a9af5e6b5bf
Prasanna Kumar Pattam <pprasannak@hotmail.com>
2017-11-27 06:19:40 +0530

    Working on Authentication

--- src/0xServices.Web/ClientApp/app/fabric/FabHttpInterceptor.ts
+++ src/0xServices.Web/ClientApp/app/fabric/FabHttpInterceptor.ts
@@ -1,11 +1,33 @@
-﻿import { Injectable } from '@angular/core';
+﻿import { Injectable, Inject } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest } from '@angular/common/http';
 import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class FabHttpInterceptor implements HttpInterceptor {
+
+    private rawApiUrl: string = '/api/';
+    private apiUrl: string;
+
+
+    constructor(@Inject('BASE_URL') private baseUrl: string) {
+        this.apiUrl = baseUrl + '/api/'
+    }
+
+
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        let url: string = req.url;
+
+        if (url.indexOf(this.rawApiUrl) === 0) {
+            url = this.baseUrl + url;
+        }
+
+        if (url.indexOf(this.apiUrl) === 0) {
+            // setting companyid in the header
+            // config.headers.companyId = $window.profile.companyId
+        }
+
         debugger;
-        return next.handle(req);
+        const reqNew = req.clone({ url: url });
+        return next.handle(reqNew);
     }
 }
---
+++ src/0xServices.Web/ClientApp/app/fabric/account/models/ChangePasswordModel.ts
@@ -0,0 +1,5 @@
+﻿export class ChangePasswordModel {
+    currentPassword: string;
+    newPassword: string;
+    confirmPassword: string;
+}
