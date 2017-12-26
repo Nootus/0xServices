af9025f11f4f1eedc8cbc121e640036164324149
Prasanna Kumar Pattam <pprasannak@hotmail.com>
2017-12-26 20:46:38 +0530

    Working on button icon click event

--- src/0xServices.Web/ClientApp/app/fabric/button/ButtonIconComponent.ts
+++ src/0xServices.Web/ClientApp/app/fabric/button/ButtonIconComponent.ts
@@ -1,4 +1,4 @@
-﻿import { Component, Input } from "@angular/core";
+﻿import { Component, Input, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: "button-icon",
@@ -8,4 +8,9 @@
 export class ButtonIconComponent {
     @Input() tooltip: string;
     @Input() tabIndex: number = -1;
+    @Output() click: EventEmitter<MouseEvent>
+
+    onClick(event: MouseEvent): void {
+        alert("clicked");
+    }
 }
